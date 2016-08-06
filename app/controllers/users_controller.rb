49daaa0bf47f3bb277b4652254848c54e89c039d
Mike Villa <mikevilla@gmail.com>
2016-08-06 14:14:26 -0700

    build out sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  # create a new User instance.
+  def new
+    @user = User.new
+  end
+
+  # Saves the user to the database. Will redisplay the form if there are any validation errors.
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_url, notice: "Welcome to the Task App!"
+    else
+      render "new"
+    end
+  end
+
 end
