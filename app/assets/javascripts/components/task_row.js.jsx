fc7b0745c99c872d502eeea31c17dad1c1d61b37
Mike Villa <mikevilla@gmail.com>
2016-08-08 16:06:03 -0700

    setup js frontend classes to be able to take in which task is to be worked on

--- app/assets/javascripts/components/task_row.js.jsx
+++ app/assets/javascripts/components/task_row.js.jsx
@@ -7,6 +7,27 @@ var TaskRowComponent = React.createClass ({
 
   componentDidMount: function() {
       console.log('componentDidMount TaskComponent');
+
+      var propsTaskID = this.props.taskRow.id;
+
+      // Event handlers, for now using jquery but later will move over to handle the event with reactjs. This is just a demo.
+      $( ".completed_" + propsTaskID ).click(function() {
+        var taskId = $(this).attr('data-id');
+        console.log('One clicked', taskId);
+
+        // send request to update this task as In Progress
+
+      });
+
+      $( ".inprogress_" + propsTaskID).click(function() {
+        var taskId = $(this).attr('data-id');
+        console.log('One clicked', taskId);
+
+       // send request to update this task as Completed
+
+
+      });
+
    },
 
   render: function() {
@@ -14,14 +35,15 @@ var TaskRowComponent = React.createClass ({
       console.log('render TaskComponent');
       console.log(this.props.taskRow);
 
-      var editUrl = "/tasks/" + this.props.taskRow.id + "/edit";
+      var status = this.props.taskRow.status.toLowerCase().replace(/ /g, ''),
+        editUrl = "/tasks/" + this.props.taskRow.id + "/edit",
+        statusClass = "awe-icon awe-icon-check " + status + "_" + this.props.taskRow.id ;
 
       return (
         <li>
-               <i className="awe-icon awe-icon-check"></i>
+               <i className={statusClass} data-id={this.props.taskRow.id}></i>
                <div>
                  <a href={editUrl}>
-
                    <i className="awe-icon awe-icon-arrow-right"></i>
                    <span className="task-title">{this.props.taskRow.title}</span>
                    <span>{this.props.taskRow.description}</span>
