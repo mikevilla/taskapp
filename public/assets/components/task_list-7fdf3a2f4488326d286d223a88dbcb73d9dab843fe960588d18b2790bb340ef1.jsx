87bc70117d469f386f37545af8602102558c0ea5
Mike Villa <mikevilla@gmail.com>
2016-08-09 01:29:36 -0700

    precompile

---
+++ public/assets/components/task_list-7fdf3a2f4488326d286d223a88dbcb73d9dab843fe960588d18b2790bb340ef1.jsx
@@ -0,0 +1,42 @@
+var TaskListComponent = React.createClass ({
+
+  getInitialState: function() {
+     console.log('getInitialState TaskListComponent 2');
+     return {taskListData: this.props.taskListData};
+  },
+
+  componentDidMount: function() {
+      console.log('componentDidMount TaskListComponent');
+   },
+
+
+  render: function() {
+    console.log('render: TaskRowComponent');
+    console.log('this.state.taskListData', this.state.taskListData);
+
+    var rows = this.state.taskListData.map((taskRow) => {
+      return (<TaskRowComponent key={taskRow.id} taskRow={taskRow} />)}
+    );
+
+    return (
+        <section className="main-task-container">
+          <div className="container">
+              <div className="row">
+                  <div className="">
+                      <div className="awe-services">
+                          <ul className="awe-services__list">
+                            {rows}
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+    );
+  }
+
+});
+
+
+
+
---
+++ public/assets/task_app-c43c147cd1b36512c23708d64d6301775aaf5b7f58a380c8f91a3cbc3369c809.jsx
@@ -0,0 +1,95 @@
+// Once document is ready then you can proceed to mount the react js components, shorthand for $(document).ready()
+$(function() {
+  console.log( "ready!" );
+
+  var apiTaskListUrl = '/api/tasks.json',
+    apiInProgressTaskListUrl = '/api/inprogress.json',
+    apiCompletedTaskListUrl = '/api/completed.json',
+    apiTaskUrl = "",
+    sortBy = 'date';
+
+  switch(status) {
+      case "Completed":
+          apiTaskUrl = apiCompletedTaskListUrl;
+          break;
+      case "In Progress":
+          apiTaskUrl = apiInProgressTaskListUrl;
+          break;
+      case "All":
+        apiTaskUrl = apiTaskListUrl;
+        break;
+      default:
+        apiTaskUrl = "";
+  }
+
+  // Initial call to populate the task data
+  refreshData(sortBy);
+
+  $(".redirectCompleted").click(function() {
+    window.location.replace("/completed");
+  });
+
+  $(".redirectInProgress").click(function() {
+    window.location.replace("/inprogress");
+  });
+
+  $(".mikeTest").click(function() {
+    console.log('testing...');
+  });
+
+  $('.button-priority').click(function() {
+    console.log('button-priority clicked');
+    sortBy = 'priority';
+    $('.button-priority').addClass('selected');
+    $('.button-date').removeClass('selected');
+    $('.taskContainerComponent').remove();
+    refreshData(sortBy);
+  });
+
+  $('.button-date').click(function() {
+    console.log('button-date clicked');
+    sortBy = 'date';
+    $('.button-date').addClass('selected');
+    $('.button-priority').removeClass('selected');
+    $('.taskContainerComponent').remove();
+    refreshData(sortBy);
+  });
+
+  function refreshData (sortBy) {
+    $.ajax({
+      url: apiTaskUrl,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+
+        // Sort the array
+        switch(sortBy) {
+            case "priority":
+                data.sort(function(a, b){return a.priority-b.priority});
+                break;
+            case "date":
+                data.sort(function(a,b){
+                  return new Date(a.target) - new Date(b.target);
+                });
+                break;
+            default:
+              data.sort(function(a,b){
+                return new Date(a.priority) - new Date(b.priority);
+              });
+              break;
+        }
+        // Data retrieved successfully so start rendering the TaskContainerComponent
+        ReactDOM.render(
+          <TaskContainerComponent url={apiTaskUrl} taskData={data}/>,
+          document.getElementById('mountTaskListComponent')
+        );
+      }.bind(this),
+      error: function(xhr, status, err) {
+       console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  };
+
+});
+
+
