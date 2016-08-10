8e56304210ee0503aa5f6bb0b15a764b941befa7
Mike Villa <mikevilla@gmail.com>
2016-08-10 12:15:12 -0700

    add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 
   # attr_accessible is a Rails method that allows you to pass in values to a mass assignment
   attr_accessible :email, :password, :password_confirmation
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
   validates_uniqueness_of :email
   has_many :tasks
 
